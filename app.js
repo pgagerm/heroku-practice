9795f5838d41148417db1447e7df14eab0f074cb
Michael Germany <mike@rickgermany.com>
2015-02-20 10:31:05 -0700

    more minor changes

--- app.js
+++ app.js
@@ -14,3 +14,4 @@ var port = process.env.PORT || 9439;
 var server = app.listen(port, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
+console.log(process.env);
